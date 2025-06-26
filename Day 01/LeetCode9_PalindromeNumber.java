5ffd8cd77bb783f0cd3ab1b3f89400253adfcf4f
Nishant Raghav <nishantxraghav@gmail.com>
2025-06-26 14:10:15 +0530

    Day 1: Added Java solution for LeetCode 9 - Palindrome Number

---
+++ Day 01/LeetCode9_PalindromeNumber.java
@@ -0,0 +1,26 @@
+// LeetCode Problem: 9. Palindrome Number
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/palindrome-number/
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+
+        int n = x;
+
+        int revNum = 0;
+
+        while(n>0){
+            int d = n%10;
+            revNum = revNum*10 + d;
+            n = n/10;
+        }
+        if(revNum == x){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
